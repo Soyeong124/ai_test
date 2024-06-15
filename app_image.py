aa7d07216bc2125e1ca9a44fe95ba0f9cb28eb36
Soyeong_Yun <devysy55@gmail.com~>
2024-06-15 17:32:16 +0900

    시나리오 봇 올리기

--- app_image.py
+++ app_image.py
@@ -1,8 +1,29 @@
-import streamlit as st
+import os
+from openai import OpenAI
+import streamlit as st # 이 순서에 유의해 주세요!, 임포트 안되어 있으면 임포트 전까지 코드에는 안먹음
 
-st.title('동물 이미지 찾아주기 😎')
+os.environ["OPENAI_API_KEY"] = st.secrets['API_KEY']
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-name = st.text_input('영어로 동물을 입력하세요')
+st.title('슈퍼 시나리오 봇🤩')
+keyword = st.text_input("키워드를 입력하세요")
 
-if st.button('찾아보기'):
-    st.image(f'https://edu.spartacodingclub.kr/random/?{name}')
+
+    
+if st.button('생성하기'):
+    with st.spinner('시나리오를 열심히 만들고 있어요'):
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": keyword,
+                },
+                {
+                    "role": "system",
+                    "content": "입력 받은 키워드에 대한 흥미진진한 300자 이내의 시나리오를 작성해줘.",
+                }
+            ],
+            model="gpt-4o",
+        )
+    result = chat_completion.choices[0].message.content
+    st.write(result)
